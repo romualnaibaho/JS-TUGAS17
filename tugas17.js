7d9327ced84139c02187e2a911d9ae7e8c0c82d3
Romual Naibaho <romualnaibaho26@gmail.com>
2021-06-30 22:54:01 +0700

    js

---
+++ tugas17.js
@@ -0,0 +1,8 @@
+
+var str1 = "Saya beLajar bahaSa peMrograman deNgan khUsuk.";
+var str2 = "Allen SedAng MemAncinG IkaN di SuNgai, DIA MEndapat IkAN GaBuS 3 Ekor dan LanGsuNg Dijual.";
+
+console.log(str1.toLowerCase());
+console.log(str1.toUpperCase());
+console.log(str2.toLowerCase());
+console.log(str2.toUpperCase());
